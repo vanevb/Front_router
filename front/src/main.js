460f6ba11920c71623ed8176aea3b78f4409596e
Vanessa V <vanevb976@gmail.com>
2021-09-03 11:46:37 -0500

    Cambios en dependencias

--- front/src/main.js
+++ front/src/main.js
@@ -1,32 +1,43 @@
-import {createApp } from 'vue'
+// import {createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+// import { createApolloProvider } from '@vue/apollo-option'
+
+// import { setContext } from 'apollo-link-context'
+
+// const httpLink = createHttpLink({
+//     uri: 'https://apigatewaylibreria.herokuapp.com/',
+// })
+
+
+// const authLink = setContext((_, { headers }) => {
+//     return {
+//         headers: {
+//             ...headers,
+//             "Authorization": localStorage.getItem("access_token") || ""
+//         }
+//     }
+// })
+
+// const apolloClient = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache()
+// })
+
+// const apolloProvider = new createApolloProvider({
+//     defaultClient: apolloClient
+// })
+// no funciona esta mal
+// createApp(App).use(router).use(apolloProvider).mount('#app')
+
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
-import { createApolloProvider } from '@vue/apollo-option'
 
-import { setContext } from 'apollo-link-context'
+Vue.config.productionTip = false
 
-const httpLink = createHttpLink({
-    uri: 'https://apigatewaylibreria.herokuapp.com/',
-})
-
-
-const authLink = setContext((_, { headers }) => {
-    return {
-        headers: {
-            ...headers,
-            "Authorization": localStorage.getItem("access_token") || ""
-        }
-    }
-})
-
-const apolloClient = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-})
-
-const apolloProvider = new createApolloProvider({
-    defaultClient: apolloClient
-})
-// no funciona esta mal
-createApp(App).use(router).use(apolloProvider).mount('#app')
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
